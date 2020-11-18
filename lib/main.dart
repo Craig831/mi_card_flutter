3379fc333f0edfa6d65b7238347219de8f3b8e76
Craig Jeffords <cjeffords831@gmail.com>
2020-11-18 17:38:31 -0600

    through sec 6 unit 8

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,75 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.only(left: 30.0),
-            padding: EdgeInsets.all(20),
-            color: Colors.white,
-            child: Text('Hello'),
-          ),
-        ),
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.red,
+              backgroundImage: AssetImage('images/avatar.jpg'),
+            ),
+            Text(
+              'Craig Jeffords',
+              style: TextStyle(
+                fontFamily: 'ArchitectsDaughter',
+                fontSize: 40,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              'SR. SOFTWARE ENGINEER',
+              style: TextStyle(
+                fontFamily: 'SourceSansPro',
+                color: Colors.teal.shade100,
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.phone, color: Colors.teal),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    '(414) 750-7104',
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.email, color: Colors.teal),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'cjeffords831@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
